fa785a092f8ded15cee83daf39ace664330488fd
Amos <gp3g4xup6@gmail.com>
2025-01-22 17:05:04 +0800

    professional condition settings

--- src/components/professionalComponents/professionalSkill.vue
+++ src/components/professionalComponents/professionalSkill.vue
@@ -1,31 +1,17 @@
 <template>
   <p class="border-[#d1d1e0] pt-20 pb-5 border-b-[3px] w-[600px] h-full text-3xl text-black text-left">Skill</p>
   <div class="w-[600px]">
-    <template v-for="(item, index) in skill" :key="index">
-      <template v-if="index % 2 === 0">
-        <div class="w-full flex justify-start items-center pl-10">
-          <div class="flex relative justify-center flex-col items-center bg-white p-5 shadow-[0_10px_20px_#00000050] w-[300px] min-h-[175px]" :class="{ 'mt-10 ': index === 0, '-mt-6': index / 2 >= 1 }">
-            <div class="flex flex-col text-center py-2 text-2xl px-4 w-full h-full text-red-600 font-black border-b-[3px]">
-              {{ item.skillType }}
-            </div>
-            <template v-for="skill in item.skillList" :key="skill.name">
-              <div class="flex w-full mt-2">◎ {{ skill.name }}</div>
-            </template>
+    <template v-for="(item, index) in skill" :key="item.skillType">
+      <div class="w-full flex justify-start items-center pl-10" :class="{ 'justify-start': isOdd(index), 'justify-end': isEven(index), 'pl-10': isOdd(index), '-mt-32': isEven(index) }">
+        <div class="flex relative justify-center flex-col items-center bg-white p-5 shadow-[0_10px_20px_#00000050] w-[300px] min-h-[175px]" :class="{ 'mt-10 ': isFirstOdd(index), '-mt-6': notFirstOdd(index) }">
+          <div class="flex flex-col text-center py-2 text-2xl px-4 w-full h-full text-red-600 font-black border-b-[3px]">
+            {{ item.skillType }}
           </div>
+          <template v-for="skill in item.skillList" :key="skill.name">
+            <div class="flex w-full mt-2">◎ {{ skill.name }}</div>
+          </template>
         </div>
-      </template>
-      <template v-else>
-        <div class="w-full flex justify-end items-center -mt-32">
-          <div class="flex relative justify-center flex-col items-center bg-white p-5 shadow-[0_10px_20px_#00000050] w-[300px] min-h-[175px]">
-            <div class="flex flex-col text-center py-2 text-2xl px-4 w-full h-full text-red-600 font-black border-b-[3px]">
-              {{ item.skillType }}
-            </div>
-            <template v-for="skill in item.skillList" :key="skill.name">
-              <div class="flex w-full mt-2">◎ {{ skill.name }}</div>
-            </template>
-          </div>
-        </div>
-      </template>
+      </div>
     </template>
   </div>
 </template>
@@ -51,4 +37,20 @@ const skill = [
     skillList: [{ name: 'Git/GitHub/GitLab' }, { name: 'Internationalization (i18n)' }, { name: 'API Documentation (Swagger)' }],
   },
 ]
+
+const isOdd = (index: number) => {
+  return index % 2 === 0
+}
+
+const isEven = (index: number) => {
+  return index % 2 !== 0
+}
+
+const isFirstOdd = (index: number) => {
+  return index === 0
+}
+
+const notFirstOdd = (index: number) => {
+  return index / 2 >= 1
+}
 </script>
