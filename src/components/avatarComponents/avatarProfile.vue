2561b8feab9c50f38d011ecfecb936a1104b23ca
Amos <gp3g4xup6@gmail.com>
2025-01-24 17:05:43 +0800

    delete  surplus code

--- src/components/avatarComponents/avatarProfile.vue
+++ src/components/avatarComponents/avatarProfile.vue
@@ -2,7 +2,7 @@
   <div class="z-10 flex justify-center items-center">
     <div class="relative min-w-[900px] h-[500px]">
       <div class="relative shadow-[-30px_20px_20px_#00000020] w-1/3 h-full">
-        <avatar :githubIcon="githubIcon" />
+        <avatar />
       </div>
       <div class="top-0 right-0 absolute bg-white shadow-[30px_-20px_50px_#00000010] w-2/3 h-full">
         <avatarResume />
@@ -12,16 +12,8 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import {} from 'vue'
 
 import avatar from '@/components/avatarComponents/avatar.vue'
 import avatarResume from '@/components/avatarComponents/avatarResume.vue'
-
-type Props = {
-  githubIcon: string | undefined
-}
-
-const props = defineProps<Props>()
-
-const githubIcon = computed(() => props.githubIcon || '')
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,18 +3,13 @@
     <div class="bg-main5 w-1/3"></div>
     <div class="bg-white w-2/3"></div>
     <div class="top-1/2 left-1/2 absolute w-[700px] w-1/3 h-[500px] translate-x-[-50%] translate-y-[-50%]">
-      <avatarProfile :githubIcon="githubIcon" />
+      <avatarProfile />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {} from 'vue'
-import { storeToRefs } from 'pinia'
-import { useCommonStore } from '@/store/commonStore'
 
 import avatarProfile from '@/components/avatarComponents/avatarProfile.vue'
-
-const commonStore = useCommonStore()
-const { githubIcon } = storeToRefs(commonStore)
 </script>
