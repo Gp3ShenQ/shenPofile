6df55983958722d7cf247b6f499fa6ca29606d97
Amos <gp3g4xup6@gmail.com>
2025-02-04 13:28:36 +0800

    project fix tips

--- src/components/projectComponents/shenProfile.vue
+++ src/components/projectComponents/shenProfile.vue
@@ -1,6 +1,8 @@
 <template>
   <p class="border-[#d1d1e0] pt-20 pb-5 border-b-[3px] w-full max-w-[600px] text-3xl text-black text-left page-title-font">Profile - Project</p>
-  <div class="w-full max-w-[600px]"></div>
+  <div class="w-full max-w-[600px] h-[300px]">
+    <p class="mt-20 text-5xl text-black text-center page-title-font">In progress</p>
+  </div>
 </template>
 
 <script setup lang="ts">
