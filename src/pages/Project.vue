d415cd31bb9e3231472fdd17f8d5619dbe33f52e
Amos <gp3g4xup6@gmail.com>
2025-02-26 10:54:10 +0800

    add project fix 檔案大小部屬問題

---
+++ src/pages/Project.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative flex flex-col justify-start items-center bg-main1 mt-[140px] w-full">
+    <div>
+      <p class="mt-10 font-black text-black text-5xl text-center page-title-font">Project</p>
+    </div>
+    <div class="flex flex-col justify-start items-center px-4 w-full">
+      <ShenProfile />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ShenProfile from '@/components/projectComponents/shenProfile.vue'
+</script>
